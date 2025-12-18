03bb529ea85dee5e344c73ebf838cadb4eeafa0e
Gajendra N L <gajendra@Gajendras-MacBook-Air.local>
2025-12-18 13:46:05 +0530

    try base design system

--- catalog-app/src/App.jsx
+++ catalog-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import { Button } from '@gnl-org/base-design-system';
 
 function App() {
   return (
     <>
       <h2>
-        Catalog 23App is running! on port 5101
+        Catalog App is running! on port 5101
       </h2>
+      <Button>Show all products</Button>
     </>
   )
 }
